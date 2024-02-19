1fcb48e1aa23d4931efadc78bbfe4b144819a264
towakey <kyosgame@gmail.com>
2024-02-19 22:14:52 +0900

    vuetifyインストール

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const drawer = ref(false)
+const menu = [
+  {
+    title: 'TOP',
+    path: ''
+  }
+]
+</script>
+<template>
+  <v-layout>
+    <v-app-bar
+    primary
+    prominent
+    >
+      <v-app-bar-nav-icon
+      @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        Pokedex-Online
+      </v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-navigation-drawer
+    v-model="drawer"
+    location="left"
+    temporary
+    >
+      <NuxtLink
+      v-for="item in menu" :key="item.title"
+      :to="{path: `/${item.path}`}"
+      >
+        <v-list nav>
+          <v-list-item
+          prepenf-icon>
+            {{ item.title }}
+          </v-list-item>
+        </v-list>
+      </NuxtLink>
+    </v-navigation-drawer>
+    <v-main
+    style="height: 100%;"
+    >
+      <slot></slot>
+    </v-main>
+  </v-layout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+</script>
+<template>
+  <h1>Pokedex-Online Index</h1>
+</template>

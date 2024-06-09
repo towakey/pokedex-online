6ada01286469ef65f1c04ca3f551dfb84af650ca
towakey <kyosgame@gmail.com>
2024-06-09 11:32:18 +0900

    pwa導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,7 @@ export default defineNuxtConfig({
     },
     //...
     "@nuxt/content",
+    '@vite-pwa/nuxt',
   ],
   sourcemap: false,
   vite: {
@@ -56,4 +57,50 @@ export default defineNuxtConfig({
     //   },
     // },
   },
-})
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Pokedex-Online',
+      short_name: 'PokedexOnline',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'icon.png',
+          sizes: '1000x1000',
+          type: 'image/png',
+        },
+        // {
+        //   src: 'icon-192x192.png',
+        //   sizes: '192x192',
+        //   type: 'image/png',
+        // },
+        // {
+        //   src: 'icon-512x512.png',
+        //   sizes: '512x512',
+        //   type: 'image/png',
+        // },
+        // {
+        //   src: 'icon-512x512.png',
+        //   sizes: '512x512',
+        //   type: 'image/png',
+        //   purpose: 'any maskable',
+        // },
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      navigateFallback: '/'
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 3600,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  }})

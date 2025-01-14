4dfe23df2ceb24610557b7a5af505b57acd8eee9
towakey <kyosgame@gmail.com>
2025-01-14 19:57:59 +0900

    タッチイベント変更コミット

--- plugins/vue-touch-events.ts
+++ plugins/vue-touch-events.ts
@@ -1,24 +1,11 @@
-// plugins/vue-touch-events.js
-// import Vue3TouchEvents from 'vue-touch-events'
-
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.vueApp.use(Vue3TouchEvents)
-// })
-
-// import Vue3TouchEvents, {
-//   type Vue3TouchEventsOptions,
-// } from "vue3-touch-events";
-
-// app.use<Vue3TouchEventsOptions>(Vue3TouchEvents, {
-//   disableClick: false
-//   // any other global options...
-// })
-
+// plugins/vue-touch-events.ts
 import Vue3TouchEvents, {
   type Vue3TouchEventsOptions,
 } from "vue3-touch-events";
 
-app.use<Vue3TouchEventsOptions>(Vue3TouchEvents, {
-  disableClick: false
-  // any other global options...
-})
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use<Vue3TouchEventsOptions>(Vue3TouchEvents, {
+    disableClick: false
+    // any other global options...
+  });
+});
